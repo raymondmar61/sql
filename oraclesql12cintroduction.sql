6c7a766ab2374175b93eec40d756494a9cb342ca
raymondmar61 <raym61@hotmail.com>
2017-06-22 13:51:12 -0700

    upload

--- oraclesql12cintroduction.sql
+++ oraclesql12cintroduction.sql
@@ -170,7 +170,7 @@ from employees; /* all a characters in first_name are replaced, not like instr i
 select first_name, lpad(first_name,10,'*')
 from employees;
 
-/* math Number or numeric functions:  round, trunc truncates, ceil, floor, mod returns remainder*/
+/* number format math Number or numeric functions:  round, trunc truncates, ceil, floor, mod returns remainder*/
 /* ceil and floor get one argument */
 select round(12.136,2) from dual; /* returns 12.14 */
 select round(12.136,3) from dual; /* returns 12.136 */
@@ -218,9 +218,9 @@ from dual; /* return 06/21/2017 which is a Wednesday ran on Tuesday 06/20/17 */
 select last_day('06/20/2017')
 from dual; /* return 06/30/2017 ran on 06/20/17 */
 
-/* to_number convert character to number */
+/* to_number convert character to number.  Change characters to a number with the given format. */
 /* to_char convert number or date to character */
-/* to_date convert character to date.  There are many format model to convert character to date; e.g. yyyy is full year in numbers, year is spelling the year, mm is two digit month, dd is two digit number day. */
+/* to_date convert character to date.  There are many format model to convert character to date; e.g. yyyy is full year in numbers, year is spelling the year, mm is two digit month, dd is two digit number day.  There are many format model to convert character to date; e.g. 9 represnts a number, 0 display 0, $ display a dollar sign, . display a decimal, , display a comma */
 select first_name, last_name, hire_date, to_char(hire_date, 'yyyy')
 from employees
 where to_char(employee_id) = 100; /* really no need to convert employee_id to character */
@@ -230,3 +230,59 @@ select first_name, hire_date, to_char(hire_date,'mon-yyyy') as "mon abbrev jan-4
 from employees;
 select first_name, hire_date, to_char(hire_date,'Mon-yyyy') as "mon abbrev Jan-4 digit year"
 from employees;
+select hire_date, to_char(hire_date, 'ddth-Mon-yyyy') as "ddth-Mon-yyyy 21st-Sep-2005"
+from employees;
+select hire_date, to_char(hire_date, 'ddth-Month-yyyy') as "ddth-Month full-yyyy 25th-June      -2005"
+from employees;
+select salary, to_char(salary, '$99.999.99') as "$24,000.00"
+from employees;
+select salary, commission_pct, to_char(salary*commission_pct, '$9.99')  
+from employees
+where commission_pct is not null; /* returns ####### because need more display numbers */
+select salary, commission_pct, to_char(salary*commission_pct, '$9,999.99')
+from employees
+where commission_pct is not null;
+select to_number('$6,152.21','$99,999.99')
+from dual; #returns 6152.21  yes, no dollar sign, no comma.  Change characters to a number with the given format.
+select first_name, last_name, to_char(hire_date, 'ddspth Month yyyy') as "Hire Date" #Hire Date twenty-fourth December    2005
+from employees
+where hire_date > to_date('Jun 25, 2005','Mon dd, YYYY');
+
+/* nvl function null if null nvl(expression1, expression2) if expression1 is null, then return expression 2*/
+select job_id, first_name, last_name, nvl(commission_pct,0) as nvlcommission, commission_pct
+from employees
+where job_id in ('SA_REP','IT_PROG');
+select salary, commission_pct, salary + (salary*commission_pct), salary + (salary*nvl(commission_pct,0))
+from employees;
+/* above or below works same results*/
+select salary, commission_pct, salary + (salary*commission_pct), nvl((salary + salary*commission_pct),salary)
+from employees;
+/* nvl2 function null if not null nvl(expression1, expression2, expression3) if expression1 is not null, then return expression 2; if expression1 is null, then return expression 3.  expression2 and expression3 must be the same data type. */
+select salary, commission_pct, salary + (salary*commission_pct), nvl2(commission_pct,'has','has not')
+from employees;
+/* nullif function nullif(expression1, expression2) compares expression1 and expression2.  If equal, return null.  If not equal, return expression1 */
+select first_name, last_name, length(first_name), length(last_name)
+from employees
+where nullif(length(first_name),length(last_name)) is null; /* lament terms, if length(first_name)=length(last_name), then return null */
+/* coalesce function coalesce(expression1, expression2, expression3, . . . expressionx) advanced nvl function.  If expression1 is null, look at expression2.  If expression2 is null, look at expression3.  Continues until any un-null value or the last expression expressionx; i.e. returns the first non null value or expressionx if previous expressions are null. */
+select state_province, city, coalesce(state_province, city, 'not has')
+from locations;
+
+/* case expression.  case expr when comparisonexpr1 then returnexpr1 [when comparisonexpr2 then returnexpr2 when comparisonexprn then returnexprn else elseexpr end].  Used for if then else or if else logic SQL. Can be used in select clause and where clause. returnexpr must be same data types */
+select first_name, last_name, job_id, salary, hire_date, case job_id when 'ST_MAN' then salary*1.2 when 'SH_MAN' then salary*1.3 when 'SA_MAN' then salary * 1.4 else salary*1.0 end as "Updated Salary"
+from employees
+where job_id in ('ST_MAN','SH_MAN','SA_MAN');
+select first_name, last_name, job_id, salary
+from employees
+where (case when job_id = 'IT_PROG' and salary > 5000 then 1
+when job_id = 'SA_MAN' and salary > 10000 then 1
+else 0
+end) = 1; /* 1 is a resultholder.  return it_prog greater than 5000 and sa_man greater than 10000 employees */
+/* above or below works same result.  above is complicated */
+select first_name, last_name, job_id, salary
+from employees
+where (job_id = 'IT_PROG' and salary > 5000) or (job_id = 'SA_MAN' and salary > 10000);
+/* decode function.  decode (columnname or expr search1, result1 ,[search2, result2, . . .,] [default]).  Oracle's specific, easy to use alternative of case expression.  if then else logic.  search and result must be same data type respectively.  expression can be different type. */
+select first_name, last_name, job_id, salary, hire_date, decode(job_id, 'ST_MAN', salary*1.2, 'SH_MAN', salary*1.3, 'SA_MAN', salary*1.4, salary*1.0) as "Updated Salary"
+from employees
+where job_id in ('ST_MAN','SH_MAN','SA_MAN');
