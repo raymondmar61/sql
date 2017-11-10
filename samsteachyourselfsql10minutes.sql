ee1aa9b19ba8e11a9fc46086295fa4e7d46bffac
raymondmar61 <raym61@hotmail.com>
2017-11-10 13:10:53 -0800

    upload

--- samsteachyourselfsql10minutes.sql
+++ samsteachyourselfsql10minutes.sql
@@ -66,4 +66,88 @@ select cust_contact
 from customers
 where cust_contact like 'J%' or cust_contact like 'M%'
 order by cust_contact;
---start page 59 pdf
+
+--Lesson 7 Creating Calculated Fields 59 (48)
+--Calculated fields don’t exist in database tables. Rather, a calculated field is created on-the-fly within a SQL SELECT statement.  It is far quicker to perform these operations on the database server than it is to perform them within the client application because DBMSs are built to perform this type of processing quickly and efficiently.
+--You concatenate columns using a special operator which can be a plus sign (+) or two pipes (||).
+select trim(vend_name) || ' (' || trim(vend_country) || ')' as vend_title
+from vendors
+order by vend_name;
+select prod_id, quantity, item_price, quantity*item_price as expanded_price
+from orderitems
+where order_num = 20008;
+
+--Lesson 8 Using Data Manipulation  Functions 68 (57)
+select trim(vend_name), upper(trim(vend_name)) as uppercase, lower(trim(vend_name)) as lowercase
+from vendors;
+select trim(vend_name), length(vend_name), length(trim(vend_name)), upper(trim(vend_name)) as uppercase
+from vendors; --length(vend_name) includes the spaces in calculation, length(trim(vend_name)) excludes the spaces in calculation
+--SOUNDEX is an algorithm that converts any string of text into an alphanumeric pattern describing the phonetic representation of that text. SOUNDEX takes into account similar sounding characters and syllables, enabling strings to be compared by how they sound rather than how they have been typed.
+select cust_name, cust_contact
+from customers
+where cust_contact = 'Michael Green';  --returns nothing
+--RM:  however
+select cust_name, cust_contact
+from customers
+where soundex(cust_contact) = soundex('Michael Green');  --returns Michelle Green as cust_contact
+--Date and times are stored in tables using datatypes, and each DBMS uses its own special varieties.
+select order_num, order_date
+from orders
+where order_date between to_date('01-01-2004') and to_date('12-31-2004');
+
+--Lesson 9 Summarizing Data 77 (66)
+--You want a summary of the data, not the actual data itself.  Returning the actual table data would be a waste of time and processing resources.  SQL features a set of five aggregate functions, which are listed in Table 9.1. These functions enable you to perform all the types of retrieval just enumerated.  AVG(), COUNT(), MAX(), MIN(), SUM().
+/*
+AVG(), MAX(), MIN(), SUM() ignores null values.
+COUNT(*) counts all rows including null.
+COUNT(columnname) ignores null values.
+*/
+select round(avg(prod_price),2) as "avg rounded two decimal places", count(prod_price) as count, max(prod_price) as max, min(prod_price) as min, sum(prod_price) as sum, sum(item_price*quantity) as total_price
+from products;
+select sum(item_price*quantity) as total_price
+from orderitems
+where order_num = 20005;
+--Performing Calculations on Multiple Columns. All the aggregate functions can be used to perform calculations on multiple columns using the standard mathematical operators.
+select avg(distinct prod_price) as "distinct avg_price"
+from products
+where vend_id = 'DLL01'; --the DISTINCT argument is used so that the average only takes into account unique prices
+--DISTINCT may only be used with COUNT() if a column name is specified. DISTINCT may not be used with COUNT(*). Similarly, DISTINCT must be used with a column name and not with a calculation or expression.
+
+--Lesson 10 Grouping Data 86 (75)
+--Grouping lets you divide data into logical sets so that you can perform aggregate calculations on each group.
+select vend_id, count(*) as num_prods
+from products
+group by vend_id;
+/*
+The GROUP BY clause instructs the DBMS to group the data and then perform the aggregate on each group rather than on the entire result set.
+GROUP BY clauses can contain as many columns as you want. This enables you to nest groups, providing you with more granular control over how data is grouped.
+If you have nested groups in your GROUP BY clause, data is summarized at the last specified group. In other words, all the columns specified are evaluated together when grouping is established (so you won’t get data back for each individual column level).
+Every column listed in GROUP BY must be a retrieved column or a valid expression (but not an aggregate function). If an expression is used in the SELECT, that same expression must be specified in GROUP BY. Aliases cannot be used.
+Most SQL implementations do not allow GROUP BY columns with variable length datatypes (such as text or memo fields).
+Aside from the aggregate calculations statements, every column in your SELECT statement must be present in the GROUP BY clause.
+If the grouping column contains a row with a NULL value, NULL will be returned as a group. If there are multiple rows with NULL values, they’ll all be grouped together.
+The GROUP BY clause must come after any WHERE clause and before any ORDER BY clause.
+The ALL Clause Some SQL implementations (such as Microsoft SQL Server) support an optional ALL clause within GROUP BY. This clause can be used to return all groups, even those that have no matching rows (in which case the aggregate would return NULL). Refer to your DBMS documentation to see if it supports ALL.
+*/
+--WHERE filters rows.  HAVING filters groups.  All techniques and options in WHERE can be applied to HAVING.  Here’s another way to look it: WHERE filters before data is grouped, and HAVING filters after data is grouped.
+select cust_id, count(*) as orders
+from orders
+group by cust_id
+having count(*) >=2;
+select vend_id, count(*) as num_prods
+from products
+where prod_price >=4
+group by vend_id
+having count(*) >=2;
+select order_num, count(*) as items
+from orderitems
+group by order_num
+having count(*) >=3
+order by items, order_num;
+--or
+select order_num, count(*) as items
+from orderitems
+group by order_num
+having count(*) >=3
+order by count(*), order_num;
+--start page 95
