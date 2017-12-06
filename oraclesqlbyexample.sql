da9536fb868b5c1ba8794d0a92465ae7aae92307
raymondmar61 <raym61@hotmail.com>
2017-12-06 13:49:29 -0800

    upload

--- oraclesqlbyexample.sql
+++ oraclesqlbyexample.sql
@@ -4,6 +4,7 @@
 --Shift+F4 opens Popup Describe window on an object
 --Ctrl+F7 quick syntax formatting
 --Ctrl+Shift+Quote toggle uppecase and lowercase
+--Ctrl+Shift+Enter appears to show query builder only
 --Press SQL button at Query Results shows the SQL statements
 --You can drag tables listed in the Connections navigator into the SQL Worksheet. When you do this, you construct a SELECT statement with all columns in the table. If desired, you can then edit the statement further.
 --You need to end each SQL statement with a semicolon (;) or type a forward slash (/) on a new line; otherwise, SQL Developer displays an error.
@@ -11,6 +12,7 @@
 --F8 is SQL History.  Can search history.
 --The Single Record View window allows you to examine one record at a time and scroll through the records.  Right mouse click anywhere in Query Results.
 --The Auto-fit menu options are very useful for formatting the Results window according to the length of the data cells or the length of the column name.  Right mouse click columns at Query Results.
+--View-->Snippets.  Snippets is a window contianing SQL functions or syntax examples.  Hovering over the function reveals a brief description of the function.  Drag the SQL to the Worksheet.  
 --Comments --for single line /* ... */ for multi-line
 
 --CHAPTER 1 SQL AND DATA 1 book page (44 pdf page)
@@ -121,4 +123,91 @@ select salutation, first_name, last_name
 from student
 where last_name = 'Grant'
 order by salutation desc, first_name asc;
---start page 133
+select salutation, first_name, last_name
+from student
+where last_name = 'Grant'
+order by 1 desc, 2 asc;  --column position.  can even sort by column position
+
+--CHAPTER 4 CHARACTER, NUMBER, AND MISCELLANEOUS FUNCTIONS 133 (176)
+select state, lower(state), lower('STate')
+from zipcode;
+select city, upper(city), state, initcap(state)
+from zipcode;
+--The LPAD and RPAD functions also transform data: They left pad and right pad strings, respectively. When you pad a string, you add to it. These functions can add characters, symbols, or even spaces to your result set.
+select city, rpad(city,20,'*') as "city fill to 20 char * right", state, lpad(state,10,'-') as "state fill to 10 char - left"
+from zipcode; --Santurce,Santurce************,PR,--------PR
+select city, rpad(city,20) as "city fill to 20 w/spaces right"
+from zipcode;  --default rpad and lpad is spaces
+select ('0001234500'), ltrim('0001234500','0') as "1234500", rtrim('0001234500','0') as "00012345", ltrim(rtrim('0001234500','0'),'0') as "12345"
+from dual;
+select ('0001234500'), trim(both '0' from '0001234500') as "12345", trim(leading '0' from '0001234500') as "1234500", trim(trailing '0' from '0001234500') as "00012345"  --RM:  both is option.  The TRIM function removes leading characters, trailing characters, or both, effectively doing the job of LTRIM and RTRIM in one function.  TRIM doesn't remove character inside a string.
+from dual;
+select ('     00012345  00 00  '), trim('     00012345  00 00  ') as "remove spaces 00012345  00 00"
+from dual;
+select last_name, substr(last_name,1, 5) as "start 1, get 5 characters", substr(last_name,6) as "start 6, get rest characters"
+from student;  --substring returns part string.  If starting_position is a negative number, Oracle starts counting from the end of the string.
+select description, instr(description, 'er') as "find 'er' position return #"
+from course
+order by 2 desc; --in string looks for the occurrence of a string inside another string, returning the starting position of the search string within the target string.  ordered by desc to see the 'er's easier.
+--same as even better
+select description, instr(description, 'er') as "find 'er' position return #"
+from course
+where instr(description, 'er') > 1;
+--INSTR(char1, char2 [,starting_position [, occurrence]]).  INSTR can take two optional input parameters. The third parameter allows you to specify the start position for the search. The fourth parameter specifies which occurrence of the string to look for. When these optional parameters are not used, the default value is 1.  char1 is string.  char2 is what to look for in string.
+select last_name, length(last_name)
+from student;  --The LENGTH function determines the length of a string, expressed as a number
+--Using functions in where and order by clauses
+select first_name, last_name
+from student
+where substr(last_name,1, 2) = 'Mo';  --uses the SUBSTR function in the WHERE clause to search for student last names that begin with 'Mo'
+--same as
+select first_name, last_name
+from student
+where last_name like 'Mo%';
+select first_name, last_name
+from student
+where instr(first_name,'.') > 0
+order by length(last_name);
+--same as
+select first_name, last_name
+from student
+where first_name like '%.%'
+order by length(last_name);
+--Nested functions
+select city, rpad(upper(city),20,'.') as "CITY fill to 20 char . right"
+from zipcode;
+select first_name
+from student;
+select first_name, instr(first_name,'.') as "find .", substr(first_name,1,instr(first_name,' ')-1) as "first name no spaces",substr(first_name,instr(first_name,'.')-1,2) as "middle initial ."
+from student
+where instr(first_name,'.') > 3;
+--same as book better because what if first name two words
+select first_name, substr(first_name, instr(first_name, '.')-1) mi, substr(first_name, 1, instr(first_name, '.')-2) first
+from student
+where instr(first_name, '.') >= 3;
+--same as my way better than book because book way first name yes spaces
+select first_name, instr(first_name,'.') as "find .", substr(first_name,1,instr(first_name,'.')-3) as "first name no spaces",substr(first_name,instr(first_name,'.')-1,2) as "middle initial ."
+from student
+where instr(first_name,'.') > 3
+--There are two methods of concatenation in Oracle: One is with the CONCAT function, the other is the concatenation operator (||), which is two vertical bars or pipe symbols.  CONCAT accepts two parameters only.
+select concat(city, state)
+from zipcode;
+select city || ', ' || state
+from zipcode;
+select replace('My replacehand is asleep', 'replacehand', 'withfoot')
+from dual;  --The replace function replaces one string with another string.  replace(char, if, then)
+--the TRANSLATE function provides a one-for-one character substitution. For instance, it allows you to determine whether all the phone numbers in the STUDENT table follow the same format. In the next query, TRANSLATE substitutes the # character for every character from 0 to 9. Then the values are checked against the ‘###-###-####’ format.
+select phone
+from student
+where translate(phone,'0123456789','#########') <> '###-###-####';
+--The SOUNDEX function allows you to compare differently spelled words that phonetically sound alike. The next query uses the SOUNDEX function to display students whose last name sounds like Martin.  sounds like.
+select student_id, last_name
+from student
+where soundex(last_name) = soundex('martin');
+/*
+The backslash (\) sign is selected as the escape character to indicate that the underscore character following the character is to be interpreted as a literal underscore and not as the wildcard underscore.
+SELECT student_id, employer
+FROM student
+WHERE employer LIKE '%B\_B%' ESCAPE '\';
+*/
+--start page 158
