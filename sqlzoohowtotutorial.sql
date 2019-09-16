8722b16451414ec4eda395e7c6f348e12832ebee
raymondmar61 <raym61@hotmail.com>
2019-09-16 15:22:40 -0700

    upload

---
+++ sqlzoohowtotutorial.sql
@@ -0,0 +1,48 @@
+#https://sqlzoo.net/
+
+#Select Reference
+#https://sqlzoo.net/wiki/SELECT_Reference
+#The select command shows data from a database.  The output is always a grid with rows and columns.
+select name, population
+from bbc
+where region = 'North America';
+#Use the concat operator to combine strings.  Some SQL databases use ||.
+select concat(region, name)
+from bbc;
+#Use the like operator as a wild card.  _ matches any single character.  % matches any character.  like operator is case insensitive.
+select name
+from bbc
+where name like 'Z%';
+#Use the union operator to list multiple select statements.  The same number of columns must be the same.
+/*
+name
+Zambia
+Zimbabwe
+Z-Trip
+Za'Mari Breedlove
+Zaa Nkweta
+Zac Benoir
+Zac Efron
+*/
+select name
+from bbc
+where name like 'Z%'
+union
+select name
+from actor
+where name like 'Z%';
+#Use an additional apostrophe as an escape character
+select *
+from bbc
+where name = 'Cote d''Ivoire';
+#Self-join each table is given an alias to treat each table differently.  Inner-join is default.
+select w.first_name as worker, b.first_name as boss
+from employee w, employee b
+where w.manager_id=b.employee_id;
+#Column names with spaces column names are enclosed in single quotes
+select 'Account Balance'
+from spacemonster;
+#Use is null for null value missing data or unknown data
+select name, gdp
+from world
+where name is null;
