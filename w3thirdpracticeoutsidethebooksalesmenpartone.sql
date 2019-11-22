354292387ca9d27ae34b9382b13f30c7277bc788
raymondmar61 <raym61@hotmail.com>
2019-11-21 20:09:17 -0800

    upload

--- w3thirdpracticeoutsidethebooksalesmenpartone.sql
+++ w3thirdpracticeoutsidethebooksalesmenpartone.sql
@@ -3,6 +3,8 @@
 #https://www.w3resource.com/sql-exercises/sql-retrieve-from-table.php
 #https://www.w3resource.com/sql-exercises/sql-boolean-operators.php
 #https://www.w3resource.com/sql-exercises/sql-wildcard-special-operators.php
+#https://www.w3resource.com/sql-exercises/sql-fromatting-output-exercises.php
+#https://www.w3resource.com/sql-exercises/sql-exercises-quering-on-multiple-table.php
 /*
 Sample table: salesman
  salesman_id |    name    |   city   | commission 
@@ -95,6 +97,14 @@ Sample table: emp_details
    733843 Mario           Saule                   63
    631548 Alan            Snappy                  27
    839139 Maria           Foster                  57
+Sample table: emp_department
+DPT_CODE DPT_NAME        DPT_ALLOTMENT
+-------- --------------- -------------
+      57 IT                      65000
+      63 Finance                 15000
+      47 HR                     240000
+      27 RD                      55000
+      89 QC                      75000
 Sample table: testtable
 col1
 --------------------------
@@ -408,6 +418,9 @@ and purch_amt not in (948.50, 1983.43);
 select *
 from salesman
 where name between like 'A%' and like 'K%';  #does it work?  No.  It appears like is invalid.
+select *
+from salesman
+where name between 'A%' and 'K%';  #does it work?  Yes.  Like is invalid.  Exclude in between statement.
 #official solution
 select *
 from salesman
@@ -420,7 +433,7 @@ where name like '[a-k]%';
 #8. Write a SQL statement to find those salesmen with all other information and name started with other than any latter within 'A' and 'L'. 
 select *
 from salesman
-where name not between 'A%' and 'L%';  #It Like is invalid.  Exclude in between statement.
+where name not between 'A%' and 'L%';  #RM:  Like is invalid.  Exclude in between statement.
 
 #9. Write a SQL statement to find that customer with all information whose name begin with the letter 'B'.
 select *
@@ -502,3 +515,287 @@ where grade is not null;
 select *
 from emp_details
 where emp_lname like 'D%';
+
+#https://www.w3resource.com/sql-exercises/sql-aggregate-functions.php
+#1. Write a SQL statement to find the total purchase amount of all orders.
+select sum(purch_amt)
+from orders;
+
+#2. Write a SQL statement to find the average purchase amount of all orders.
+select avg(purch_amt)
+from orders;
+
+#3. Write a SQL statement to find the number of salesmen currently listing for all of their customers.
+select count(name)
+from salesman;
+
+#4. Write a SQL statement know how many customer have listed their names.
+select count(cust_name)
+from customer;
+
+#5. Write a SQL statement find the number of customers who gets at least a gradation for his/her performance.
+select count(grade)
+from customer
+where grade is not null;
+
+#6. Write a SQL statement to get the maximum purchase amount of all the orders.
+select max(purch_amt)
+from orders;
+
+#7. Write a SQL statement to get the minimum purchase amount of all the orders.
+select min(purch_amt)
+from orders;
+
+#8. Write a SQL statement which selects the highest grade for each of the cities of the customers.
+select city, max(grade)
+from customer
+group by city;
+
+#9. Write a SQL statement to find the highest purchase amount ordered by the each customer with their ID and highest purchase amount.
+select customer_id, max(purch_amt)
+from orders
+group by customer_id;
+
+#10. Write a SQL statement to find the highest purchase amount ordered by the each customer on a particular date with their ID, order date and highest purchase amount.
+select customer_id, ord_date, max(purch_amt)
+from orders
+group by customer_id, ord_date;
+
+#11. Write a SQL statement to find the highest purchase amount on a date '2012-08-17' for each salesman with their ID.
+select salesman_id, max(purch_amt)
+from orders
+where ord_date = '2012-08-17'
+group by salesman_id;
+
+#12. Write a SQL statement to find the highest purchase amount with their ID and order date, for only those customers who have highest purchase amount in a day is more than 2000.
+select ord_date, ord_no, max(purch_amt)
+from orders
+group by ord_date, ord_no
+having max(purch_amt) > 2000;
+#official solution
+select customer_id, ord_date, max(purch_amt)
+from orders
+group by customer_id, ord_date
+having max(purch_amt) > 2000;
+
+#13. Write a SQL statement to find the highest purchase amount with their ID and order date, for those customers who have a higher purchase amount in a day is within the range 2000 and 6000.
+select customer_id, ord_date, max(purch_amt)
+from orders
+group by customer_id, ord_date
+having max(purch_amt) between 2000 and 6000;
+
+#14. Write a SQL statement to find the highest purchase amount with their ID and order date, for only those customers who have a higher purchase amount in a day is within the list 2000, 3000, 5760 and 6000.
+select customer_id, ord_date, max(purch_amt)
+from orders
+group by customer_id, ord_date
+having max(purch_amt) in (2000, 3000, 5760, 6000);
+
+#15. Write a SQL statement to find the highest purchase amount with their ID, for only those customers whose ID is within the range 3002 and 3007.
+select customer_id, max(purch_amt)
+from orders
+where customer_id between 3002 and 3007
+group by customer_id;
+
+#16. Write a SQL statement to display customer details (ID and purchase amount) whose IDs are within the range 3002 and 3007 and highest purchase amount is more than 1000.
+select customer_id, max(purch_amt)
+from orders
+where customer_id between 3002 and 3007
+group by customer_id
+having max(purch_amt) > 1000;
+
+#17. Write a SQL statement to find the highest purchase amount with their ID, for only those salesmen whose ID is within the range 5003 and 5008.
+select customer_id, max(purch_amt)
+from orders
+where salesman_id between 5003 and 5008
+group by customer_id;
+
+#18. Write a SQL statement that counts all orders for a date August 17th, 2012.
+select count(*)
+from orders
+where ord_date = '2012-08-17';
+
+#19. Write a SQL statement that count the number of salesmen for whom a city is specified. Note that there may be spaces or no spaces in the city column if no city is specified.
+select city, count(city)
+from salesman
+group by city;
+
+#20. Write a query that counts the number of salesmen with their order date and ID registering orders for each day.
+select salesman_id, ord_date, count(*)
+from orders
+group by salesman_id, ord_date
+order by salesman_id asc, ord_date desc;
+
+#21. Write a SQL query to calculate the average price of all the products.
+select avg(pro_price)
+from item_mast;
+
+#22. Write a SQL query to find the number of products with a price more than or equal to Rs.350.
+select count(*)
+from item_mast
+where pro_price >= 350;
+
+#23. Write a SQL query to display the average price of each company's products, along with their code.  #RM:  code is pro_com
+select pro_com, avg(pro_price)
+from item_mast
+group by pro_com;
+
+#24. Write a query in SQL to find the sum of the allotment amount of all departments.
+select sum(dpt_allotment)
+from emp_department;
+
+#25. Write a query in SQL to find the number of employees in each department along with the department code.
+select emp_dept, count(*)
+from emp_details
+group by emp_dept;
+
+#https://www.w3resource.com/sql-exercises/sql-fromatting-output-exercises.php
+#1. Write a SQL statement to display the commission with the percent sign ( % ) with salesman ID, name and city columns for all the salesmen.
+select commission*100 || '%' as "Commission Percentage double quote", salesman_id, name, city
+from salesman;
+
+#2. Write a SQL statement to find out the number of orders booked for each day and display it in such a format like "For 2001-10-10 there are 15 orders".
+select 'For ' || ord_date || ' there are ' || count(*) || ' orders.' as "Orders"
+from orders
+group by ord_date
+order by ord_date;
+#SQL code below doesn't work.  Returns second column only such as 2 orders and 1 orders.
+select 'For ' || ord_date || ' there are ', count(*) || ' orders.'
+from orders
+group by ord_date;
+
+#3. Write a query to display the orders according to the order number arranged by ascending order.
+select *
+from orders
+order by ord_no;
+
+#4. Write a SQL statement to arrange the orders according to the order date in such a manner that the latest date will come first then previous dates.
+select *
+from orders
+order by ord_date desc;
+
+#5. Write a SQL statement to display the orders with all information in such a manner that, the older order date will come first and the highest purchase amount of same day will come first.
+select *
+from orders
+order by ord_date asc, purch_amt desc;
+
+#6. Write a SQL statement to display the customer name, city, and grade, etc. and the display will be arranged according to the smallest customer ID.
+select cust_name, city, grade
+from customer
+order by customer_id;
+
+#7. Write a SQL statement to make a report with salesman ID, order date and highest purchase amount in such an arrangement that, the smallest salesman ID will come first along with their smallest order date.
+select salesman_id, ord_date, max(purch_amt)
+from orders
+order by salesman_id, ord_date;
+
+#8. Write a SQL statement to display customer name, city and grade in such a manner that, the customer holding highest grade will come first.
+select cust_name, city, grade
+from customer
+order by grade desc;
+
+#9. Write a SQL statement to make a report with customer ID in such a manner that, the largest number of orders booked by the customer will come first along with their highest purchase amount.  #RM:  multiple aggregate multiple aggregate functions
+select customer_id, count(*), max(purch_amt)
+from orders
+group by customer_id
+order by count(*) desc;
+
+#10. Write a SQL statement to make a report with order date in such a manner that, the latest order date will come last along with the total purchase amount and total commission (15% for all salesmen) for that date.
+select ord_date, sum(purch_amt), sum(purch_amt*.15) as "total commission 15%"
+from orders
+group by ord_date
+order by ord_date;
+
+#https://www.w3resource.com/sql-exercises/sql-exercises-quering-on-multiple-table.php
+#1. Write a query to find those customers with their name and those salesmen with their name and city who lives in the same city.
+select c.cust_name, s.name
+from customers c join salesman s
+on c.salesman_id = s.salesman_id
+and c.city = s.city; #incorrect, want to match customer city and salesmen city only.
+#correct solution
+select c.cust_name, s.name, c.city
+from customer c, salesman s
+where c.city = s.city;
+#user solution
+select c.cust_name, s.name, c.city
+from customer c inner join salesman s
+on c.city = s.city;
+
+#2. Write a SQL statement to find the names of all customers along with the salesmen who works for them.
+select c.cust_name, s.name
+from customers c join salesman s
+on c.salesman_id = s.salesman_id;
+#also
+select c.cust_name, s.name
+from customer c, salesman s
+where c.salesman_id = s.salesman_id;
+
+#3. Write a SQL statement to display all those orders by the customers not located in the same cities where their salesmen live.
+select o.*
+from orders o join customer c
+on o.customer_id = c.customer_id
+join salesman s
+on s.salesman_id = c.salesman_id
+and s.city <> c.city;  #incorrect.  Return nothing.
+#official solution
+select o.*
+from salesman s, customer c, orders o
+where c.city <> s.city
+and o.customer_id = c.customer_id
+and o.salesman_id = s.salesman_id;  #correct
+#user solution
+select o.*
+from orders o inner join customer c
+on o.customer_id = c.customer_id
+inner join salesman s
+on o.salesman_id = s.salesman_id
+where c.city <> s.city;
+
+#4. Write a SQL statement that finds out each order number followed by the name of the customers who made the order.
+select o.ord_no, c.cust_name
+from orders o join customer c
+on o.customer_id = c.customer_id;
+#also
+select o.ord_no, c.cust_name
+from orders o, customer c
+where o.customer_id = c.customer_id;
+
+#5. Write a SQL statement that sorts out the customer and their grade who made an order. Each of the customers must have a grade and served by at least a salesman, who belongs to a city.
+select c.cust_name, c.grade, s.name
+from customer c join orders o
+on c.customer_id = o.customer_id
+join salesman_id s
+on s.salesman_id = o.salesman_id
+and s.city is not null
+and c.grade is not null;
+#also
+select c.cust_name, c.grade, o.ord_no
+from customer c, orders o, salesman s
+where c.customer_id = o.customer_id
+and c.salesman_id = s.salesman_id
+and s.city is not null
+and c.grade is not null;
+
+#6. Write a query that produces all customers with their name, city, salesman and commission, who served by a salesman and the salesman works at a rate of the commission within 12% to 14%.
+select c.cust_name, c.city, s.name, s.commission
+from customer c join salesman s
+on c.salesman_id = s.salesman_id
+where commission between 0.12 and 0.14;
+#also
+select c.cust_name, c.city, s.name, s.commission
+from customer c, salesman s
+where c.salesman_id = s.salesman_id
+and s.commission between 0.12 and 0.14;
+
+#7. Write a SQL statement that produces all orders with the order number, customer name, commission rate and earned commission amount for those customers who carry their grade is 200 or more and served by an existing salesman.
+select o.ord_no, c.cust_name, s.commission, (s.commission*o.purch_amt) as "Earned Commission"
+from orders o join customer c
+on o.customer_id = c.customer_id
+join salesman s
+on s.salesman_id = c.salesman_id
+where c.grade >= 200;
+#also
+select c.cust_name, c.grade, s.name, s.commission, (s.commission*o.purch_amt) as "earned commission", o.ord_no
+from customer c, salesman s, orders o
+where c.salesman_id = s.salesman_id
+and c.customer_id = o.customer_id
+and c.grade >= 200;
