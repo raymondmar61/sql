d44233574b4dbd4c9012a45d10f40d0db15166e4
raymondmar61 <raym61@hotmail.com>
2022-11-28 13:08:01 -0800

    Database commands

---
+++ completemysqlbeginnertoexpert01.sql
@@ -0,0 +1,12 @@
+/* Complete MySQL Beginner to Expert [en6YPAgc6WM] */
+create database helloworlddb;
+show databases;
+drop database helloworlddb;
+use helloworlddb;
+select helloworlddb(); /* video showed active database is helloworlddb.  MySQL Workbench error message Error Code: 1305. FUNCTION helloworlddb.helloworlddb does not exist	0.00023 sec*/
+use giraffe;
+select database();
+/*
+# database()
+'giraffe'
+*/
