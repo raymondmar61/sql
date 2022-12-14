417bd68b5d2a1c987147e099e354b3d6f325aa24
raymondmar61 <raym61@hotmail.com>
2022-12-14 00:57:01 -0800

    Create tables, insert values, drop tables

--- completemysqlbeginnertoexpert01.sql
+++ completemysqlbeginnertoexpert01.sql
@@ -10,3 +10,175 @@ select database();
 # database()
 'giraffe'
 */
+create database catapp;
+use catapp; /* Activate database, go to database */
+create table cats
+(name varchar(100), age int);
+show tables;
+/*
+# Tables_in_catapp
+'cats'
+*/
+show columns from cats;
+/*
+# Field, Type, Null, Key, Default, Extra
+name, varchar(100), YES, , , 
+age, int, YES, , , 
+*/
+#or
+desc cats;
+/*
+# Field, Type, Null, Key, Default, Extra
+name, varchar(100), YES, , , 
+age, int, YES, , , 
+*/
+drop table cats; /* Delete table */
+create table pastries
+(name varchar(50), quantity int);
+desc pastries;
+/*
+# Field, Type, Null, Key, Default, Extra
+name, varchar(50), YES, , , 
+quantity, int, YES, , , 
+*/
+drop table pastries;
+create table cats
+(name varchar(50), age int);
+insert into cats (name, age)
+values ("Blue",1);
+insert into cats (age, name)
+values (11,"Draco");
+insert into cats
+values ("Peanut",2),('Butter',4),("Jelly",7); /* Single quotes or double quotes valid */
+select *
+from cats;
+/*
+# name	age
+Blue	1
+Draco	11
+Peanut	2
+Butter	4
+Jelly	7
+*/
+create table people
+(firstname varchar(20), lastname varchar(20), age int);
+insert into people
+values("Tina","Belcher",13),("Bob","Belcher",42),("Linda","Belcher",45),("Phillip","Frond",38),("Calvin","Fischoeder",70);
+select *
+from people;
+/*
+Tina	Belcher	13
+Bob	Belcher	42
+Linda	Belcher	45
+Phillip	Frond	38
+Calvin	Fischoeder	70
+*/
+drop table people;
+#Null means the value is unknown.  Null is not zero.  Null is not blank.
+insert into cats (name)
+values("Alabama");
+select *
+from cats;
+/*
+# name	age
+Blue	1
+Draco	11
+Peanut	2
+Butter	4
+Jelly	7
+Alabama	NULL
+*/
+create table cats2
+(name varchar(100) not null, age int not null);
+desc cats2;
+/*
+# Field	Type	Null	Key	Default	Extra
+name	varchar(100)	NO			
+age	int	NO			
+*/
+insert into cats2(name, age)
+values("Texas",);
+show warnings;
+/*
+# Level	Code	Message
+Error	1064	You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near ')' at line 2
+*/
+create table cats3
+(name varchar(20) default 'no name provided', age int default 99);
+desc cats3;
+/*
+# Field	Type	Null	Key	Default	Extra
+name	varchar(20)	YES		no name provided	
+age	int	YES		99	
+*/
+insert into cats3(age)
+values(13);
+insert into cats3()
+values();
+insert into cats3()
+values("Montana",null);
+select *
+from cats3;
+/*
+# name	age
+no name provided	13
+no name provided	99
+Montana	null
+*/
+create table cats4
+(name varchar(100) not null default 'no name provided', age int not null default 99);
+desc cats4;
+/*
+# Field	Type	Null	Key	Default	Extra
+name	varchar(100)	NO		no name provided	
+age	int	NO		99	
+*/
+insert into cats4(name, age)
+values("Cali",NULL);
+show warnings;
+/*
+# Level	Code	Message
+Error	1048	Column 'age' cannot be null
+*/
+#Primary key.  Unique identifier unique id.
+create table uniquecats
+(catid int not null auto_increment, name varchar(100), age int, primary key (catid));
+#or
+create table uniquecats
+(catid int not null auto_increment primary key, name varchar(100), age int);
+desc uniquecats;
+/*
+# Field	Type	Null	Key	Default	Extra
+catid	int	NO	PRI		auto_increment
+name	varchar(100)	YES			
+age	int	YES			
+*/
+insert into uniquecats(name, age)
+values("Fred",23),("Louise",3);
+select *
+from uniquecats;
+/*
+# catid	name	age
+1	Fred	23
+2	Louise	3
+*/
+create table employees
+(id int not null auto_increment, lastname varchar(255) not null, firstname varchar(255) not null, middlename varchar(255), age integer not null, currentstatus varchar(100) not null default "employed", primary key(id));
+desc employees;
+/*
+# Field	Type	Null	Key	Default	Extra
+id	int	NO	PRI		auto_increment
+lastname	varchar(255)	NO			
+firstname	varchar(255)	NO			
+middlename	varchar(255)	YES			
+age	int	NO			
+currentstatus	varchar(100)	NO		employed	
+*/
+insert into employees(lastname, firstname, age)
+values("Smith","Dora",58);
+select *
+from employees;
+/*
+# id	lastname	firstname	middlename	age	currentstatus
+1	Smith	Dora		58	employed
+*/
