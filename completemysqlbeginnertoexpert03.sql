37068190e064560de45826eab0ec1cdc1f14363c
raymondmar61 <raym61@hotmail.com>
2023-04-04 19:21:32 -0700

    Many to many relationships

--- completemysqlbeginnertoexpert03.sql
+++ completemysqlbeginnertoexpert03.sql
@@ -264,3 +264,169 @@ Caleb	67.5000	FAILING
 Raj	0.0000	add case when null to be explicit
 Lisa	0.0000	add case when null to be explicit
 */
+create database relationships;
+show databases;
+use relationships;
+create table reviewers (id int auto_increment primary key, firstname varchar(100), lastname varchar(100));
+create table series (id int auto_increment primary key, title varchar(100), releasedyear year(4), genre varchar(100));
+drop table series;
+#Copy code to insert data D:\SQL\mysql-bootcamp-master\relationships\tv-review-app
+insert into series (title, releasedyear, genre) values ('Archer', 2009, 'Animation'), ('Arrested Development', 2003, 'Comedy'), ("Bob's Burgers", 2011, 'Animation'), ('Bojack Horseman', 2014, 'Animation'), ("Breaking Bad", 2008, 'Drama'), ('Curb Your Enthusiasm', 2000, 'Comedy'), ("Fargo", 2014, 'Drama'), ('Freaks and Geeks', 1999, 'Comedy'), ('General Hospital', 1963, 'Drama'), ('Halt and Catch Fire', 2014, 'Drama'), ('Malcolm In The Middle', 2000, 'Comedy'), ('Pushing Daisies', 2007, 'Comedy'), ('Seinfeld', 1989, 'Comedy'), ('Stranger Things', 2016, 'Drama');
+insert into reviewers (firstname, lastname) values ('Thomas', 'Stoneman'), ('Wyatt', 'Skaggs'), ('Kimbra', 'Masters'), ('Domingo', 'Cortes'), ('Colt', 'Steele'), ('Pinkie', 'Petit'), ('Marlon', 'Crafford');
+create table reviews (id int auto_increment primary key, rating decimal (2,1), seriesid int, reviewerid int, foreign key(seriesid) references series(id), foreign key(reviewerid) references reviewers(id)); #rating decimal (2,1) is from 0.0 to 9.9.  No 10.0.
+insert into reviews(seriesid, reviewerid, rating) values (1,1,8.0),(1,2,7.5),(1,3,8.5),(1,4,7.7),(1,5,8.9), (2,1,8.1),(2,4,6.0),(2,3,8.0),(2,6,8.4),(2,5,9.9), (3,1,7.0),(3,6,7.5),(3,4,8.0),(3,3,7.1),(3,5,8.0), (4,1,7.5),(4,3,7.8),(4,4,8.3),(4,2,7.6),(4,5,8.5), (5,1,9.5),(5,3,9.0),(5,4,9.1),(5,2,9.3),(5,5,9.9), (6,2,6.5),(6,3,7.8),(6,4,8.8),(6,2,8.4),(6,5,9.1), (7,2,9.1),(7,5,9.7), (8,4,8.5),(8,2,7.8),(8,6,8.8),(8,5,9.3), (9,2,5.5),(9,3,6.8),(9,4,5.8),(9,6,4.3),(9,5,4.5), (10,5,9.9), (13,3,8.0),(13,4,7.2), (14,2,8.5),(14,3,8.9),(14,4,8.9);
+select s.title, reviews.rating
+from series s join reviews
+on s.id = reviews.seriesid
+limit 10;
+/*
+# title	rating
+Archer	8.0
+Archer	7.5
+Archer	8.5
+Archer	7.7
+Archer	8.9
+Arrested Development	8.1
+Arrested Development	6.0
+Arrested Development	8.0
+Arrested Development	8.4
+Arrested Development	9.9
+*/
+select s.title, avg(reviews.rating)
+from series s join reviews
+on s.id = reviews.seriesid
+group by s.title
+order by avg(reviews.rating) asc;
+/*
+# title	avg(reviews.rating)
+General Hospital	5.38000
+Bob's Burgers	7.52000
+Seinfeld	7.60000
+Bojack Horseman	7.94000
+Arrested Development	8.08000
+Archer	8.12000
+Curb Your Enthusiasm	8.12000
+Freaks and Geeks	8.60000
+Stranger Things	8.76667
+Breaking Bad	9.36000
+Fargo	9.40000
+Halt and Catch Fire	9.90000
+*/
+select reviewers.firstname, reviewers.lastname, reviews.rating
+from reviewers join reviews
+on reviewers.id = reviews.reviewerid
+limit 10;
+/*
+# firstname	lastname	rating
+Thomas	Stoneman	8.0
+Thomas	Stoneman	8.1
+Thomas	Stoneman	7.0
+Thomas	Stoneman	7.5
+Thomas	Stoneman	9.5
+Wyatt	Skaggs	7.5
+Wyatt	Skaggs	7.6
+Wyatt	Skaggs	9.3
+Wyatt	Skaggs	6.5
+Wyatt	Skaggs	8.4
+*/
+#Find series with no reviews
+select *
+from series left join reviews
+on series.id = reviews.seriesid;
+/*
+# id	title	releasedyear	genre	id	rating	seriesid	reviewerid
+9	General Hospital	1963	Drama	37	5.5	9	2
+9	General Hospital	1963	Drama	38	6.8	9	3
+9	General Hospital	1963	Drama	39	5.8	9	4
+9	General Hospital	1963	Drama	40	4.3	9	6
+9	General Hospital	1963	Drama	41	4.5	9	5
+10	Halt and Catch Fire	2014	Drama	42	9.9	10	5
+11	Malcolm In The Middle	2000	Comedy				
+12	Pushing Daisies	2007	Comedy				
+13	Seinfeld	1989	Comedy	43	8.0	13	3
+13	Seinfeld	1989	Comedy	44	7.2	13	4
+*/
+select *
+from series left join reviews
+on series.id = reviews.seriesid
+where reviews.rating is null;
+#where reviews.seriesid is null and reviews.reviewerid is null; #also works
+/*
+# id	title	releasedyear	genre	id	rating	seriesid	reviewerid
+11	Malcolm In The Middle	2000	Comedy				
+12	Pushing Daisies	2007	Comedy				
+*/
+select series.genre, round(avg(reviews.rating),2)
+from series join reviews
+on series.id = reviews.seriesid
+group by series.genre
+order by 1 asc;
+/*
+# genre	avg(reviews.rating)
+Animation	7.86
+Comedy	8.16
+Drama	8.04
+*/
+select reviewers.firstname, reviewers.lastname, count(reviews.reviewerid) as count, ifnull(min(reviews.rating),0) as min, ifnull(max(reviews.rating),0) as max, round(ifnull(avg(reviews.rating),0),2) as avg, if(count(rating)>1,'Active','Inactive') as 'Status If', case when count(reviews.reviewerid) >=10 then 'Power User' when count(reviews.reviewerid) >1 then 'Active' else 'Inactive' end as 'Status Case'
+from reviewers left join reviews
+on reviewers.id = reviews.reviewerid
+group by 1, 2;
+/*
+# firstname	lastname	count	min	max	avg	Status If	Status Case
+Thomas	Stoneman	5	7.0	9.5	8.02	Active	Active
+Wyatt	Skaggs	9	5.5	9.3	7.80	Active	Active
+Kimbra	Masters	9	6.8	9.0	7.99	Active	Active
+Domingo	Cortes	10	5.8	9.1	7.83	Active	Power User
+Colt	Steele	10	4.5	9.9	8.77	Active	Power User
+Pinkie	Petit	4	4.3	8.8	7.25	Active	Active
+Marlon	Crafford	0	0.0	0.0	0.00	Inactive	Inactive
+*/
+select series.title, reviews.rating, concat(reviewers.firstname, " ",reviewers.lastname) as reviewer
+from series left join reviews
+on series.id = reviews.seriesid
+left join reviewers
+on reviewers.id = reviews.reviewerid
+limit 15;
+/*
+# title	rating	reviewer
+Archer	8.0	Thomas Stoneman
+Archer	7.5	Wyatt Skaggs
+Archer	8.5	Kimbra Masters
+Archer	7.7	Domingo Cortes
+Archer	8.9	Colt Steele
+Arrested Development	8.1	Thomas Stoneman
+Arrested Development	6.0	Domingo Cortes
+Arrested Development	8.0	Kimbra Masters
+Arrested Development	8.4	Pinkie Petit
+Arrested Development	9.9	Colt Steele
+Bob's Burgers	7.0	Thomas Stoneman
+Bob's Burgers	7.5	Pinkie Petit
+Bob's Burgers	8.0	Domingo Cortes
+Bob's Burgers	7.1	Kimbra Masters
+Bob's Burgers	8.0	Colt Steele
+*/
+#official solution
+select series.title, reviews.rating, concat(reviewers.firstname, " ",reviewers.lastname) as reviewer
+from reviewers join reviews
+on reviewers.id = reviews.reviewerid
+join series
+on series.id = reviews.seriesid
+limit 15;
+/*
+# title	rating	reviewer
+Archer	8.0	Thomas Stoneman
+Arrested Development	8.1	Thomas Stoneman
+Bob's Burgers	7.0	Thomas Stoneman
+Bojack Horseman	7.5	Thomas Stoneman
+Breaking Bad	9.5	Thomas Stoneman
+Archer	7.5	Wyatt Skaggs
+Bojack Horseman	7.6	Wyatt Skaggs
+Breaking Bad	9.3	Wyatt Skaggs
+Curb Your Enthusiasm	6.5	Wyatt Skaggs
+Curb Your Enthusiasm	8.4	Wyatt Skaggs
+Fargo	9.1	Wyatt Skaggs
+Freaks and Geeks	7.8	Wyatt Skaggs
+General Hospital	5.5	Wyatt Skaggs
+Stranger Things	8.5	Wyatt Skaggs
+Archer	8.5	Kimbra Masters
+*/
